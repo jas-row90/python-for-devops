55f7a4d62c7e1d725070ca0343cbd2adf78e7a9e
jas-row90 <101437021+jas-row90@users.noreply.github.com>
2024-07-26 19:42:04 -0500

    Create calculator_new.py

---
+++ Day-05/calculator_new.py
@@ -0,0 +1,24 @@
+def add(num1, num2):
+  add = num1 + num2
+  return m
+def sub(num1, num2):
+  sub = num1 - num2
+  return sub
+def mul(num1, num2):
+  mul = num1 * num2
+  return mul
+
+num1 = float(sys.argv[1])
+operation = sys.arg[2]
+num2 = float(sys.arg[3])
+
+if operation == "add":
+  output = add(num1, num2)
+  print(output)
+
+import os 
+
+print(os.getenv("password"))
+print(os.getenv("apitoken")
+      
+    
