4733a83335d26ee9470288273fd30a55e3c92607
jas-row90 <101437021+jas-row90@users.noreply.github.com>
2024-07-26 23:52:00 -0500

    Create test_lists_S3.py

---
+++ Day-08/test_lists_S3.py
@@ -0,0 +1,8 @@
+# lists
+
+s3_buckets_lists = ["Jas_demo_bucket", "Crystal_demo_bucket", "Jason_demo_bucket", "Rosa_demo_bucket", "Joselyn_demo_bucket"]
+print(s3_buckets_lists)
+print(type[s3_buckets_lists])
+
+
+# To make this list a touple (mutable)
