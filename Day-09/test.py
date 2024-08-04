d6562e03174a77fe55a96278f6be56e78114227e
jas-row90 <101437021+jas-row90@users.noreply.github.com>
2024-08-04 16:09:15 -0500

    Update test.py

--- Day-09/test.py
+++ Day-09/test.py
@@ -1,12 +1,16 @@
 for i in range(12):
     print("Jas")
-    print("Rvimowlett")
+    print("Rowlett")
 
 for i in range(10):
     print(i)
     print("-JRow")
-
+    
 colors = ["yellow", "green", "blue"]
-
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+           ]
 for x in colors:
+    print(x)
+
+for x in numbers:
     print(x)
